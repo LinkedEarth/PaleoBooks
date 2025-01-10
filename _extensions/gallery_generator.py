8c7530f92e8b04a0e5658d4ad47c63a110f2848d
Jordan Landers <10099739+jordanplanders@users.noreply.github.com>
2025-01-09 22:44:48 -0800

    Update gallery_generator.py

--- _extensions/gallery_generator.py
+++ _extensions/gallery_generator.py
@@ -101,7 +101,7 @@ def generate_repo_dicts(all_items):
         if len(item['cookbook_loc']) == 0:
             cookbook_loc = f"{host}/{repo}".strip().lstrip('/')
         cookbook_url = f"{cookbook_loc}/{landingpage}.html".strip()
-        print('cookbook_loc', cookbook_loc, cookbook_url)
+        # print('cookbook_loc', cookbook_loc, cookbook_url)
         master_tags = {}
         # Get information from _config (title, description, authors)
         # try:
